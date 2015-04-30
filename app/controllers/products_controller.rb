14e22b5d5d98ea332efa13c063da8cadebd869e5
Tatsuya NOGUCHI <tatsuyano@gmail.com>
2015-04-30 11:26:06 +0900

    user_idをpermitから外し、save時にcurrent_userから取得するように変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,6 +26,7 @@ class ProductsController < ApplicationController
   # POST /products.json
   def create
     @product = Product.new(product_params)
+    @product.user_id = current_user.id
 
     respond_to do |format|
       if @product.save
@@ -70,6 +71,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:user_id, :image)
+      params.require(:product).permit(:image)
     end
 end
