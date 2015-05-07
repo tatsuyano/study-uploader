d7d689b9cdb58d116c79bfc2ecf16e0423dc2cad
Tatsuya NOGUCHI <tatsuyano@gmail.com>
2015-05-07 18:12:58 +0900

    routes整理(homeコントローラ削除)

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 Rails.application.routes.draw do
+
+  root to: 'products#index'
+
+  devise_for :users, path_names: { sign_in: "login", sign_out: "logout"},
+    controllers: { omniauth_callbacks: "omniauth_callbacks" }
+
   get  '/products/',    to: 'products#index'
   get  '/products/new', to: 'products#new'
   get  '/products/:id', to: 'products#destroy'
   post '/products/',    to: 'products#create'  
-
-  root to: "home#index"
-  get 'home/index'
-  get 'home/show'
-
-  devise_for :users, path_names: { sign_in: "login", sign_out: "logout"},
-    controllers: { omniauth_callbacks: "omniauth_callbacks" }
 end
