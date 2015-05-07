74e5181bc1243babe2fa8d6e0f8ef979f7180b64
Tatsuya NOGUCHI <tatsuyano@gmail.com>
2015-05-07 16:22:06 +0900

    s3など遅いサーバーにアップする際、ファイル名に日付を使うと、メイン画像とサムネイルの名前がずれてしまう問題

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -51,11 +51,9 @@ class ImageUploader < CarrierWave::Uploader::Base
     super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
   end
 
-  # ファイル名は日本語が入ってくると嫌なので、下記のようにしてみてもいい。
-  # 日付(20131001.jpgみたいなファイル名)で保存する
   def filename
     time = Time.now
-    name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
+    name = time.strftime('%Y%m%d%H%M') + '.jpg'
     name.downcase
   end
 end
